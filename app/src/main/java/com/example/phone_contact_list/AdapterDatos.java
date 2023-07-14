1cd72eb3ab11741afc23e3efc57a40e0e7d5d28e
Juli <juliangasal46@gmail.com>
2023-07-14 15:15:01 +0200

    V.0.2: Modified, now in Java, read contacts info. TODO: Try to load a default picture

---
+++ app/src/main/java/com/example/phone_contact_list/AdapterDatos.java
@@ -0,0 +1,72 @@
+package com.example.phone_contact_list;
+
+import android.net.Uri;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class AdapterDatos extends RecyclerView.Adapter<AdapterDatos.ViewHolderDatos> {
+
+    private ArrayList<String> listDatosNombre, listDatosNumber, listDatosFoto;
+
+    public AdapterDatos(ArrayList<String> listDatosNombre,
+                        ArrayList<String> listDatosNumber,
+                        ArrayList<String> listDatosFoto){
+        this.listDatosNombre = listDatosNombre;
+        this.listDatosNumber = listDatosNumber;
+        this.listDatosFoto = listDatosFoto;
+    }
+
+    @Override
+    public ViewHolderDatos onCreateViewHolder( ViewGroup parent, int viewType) {
+        // Enlaza nuestro adaptador con el contactPaint.xml
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.contactpaint, null , false);
+        return new ViewHolderDatos(view);
+    }
+
+    @Override
+    public void onBindViewHolder(AdapterDatos.ViewHolderDatos holder, int position) {
+        // Se encarga de la comunicación entre el ViewHolder y el adaptador, usaremos el objeto Holder
+
+        holder.asignarDatos(listDatosNombre.get(position),
+                    listDatosNumber.get(position),
+                    listDatosFoto.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return listDatosNombre.size(); // Mismo tamaño , porque todos los contacts tienen nombre
+    }
+
+    public class ViewHolderDatos extends RecyclerView.ViewHolder{
+
+        TextView name, number;
+        ImageView foto;
+
+        public ViewHolderDatos(View itemView) {
+            super(itemView);
+            name = itemView.findViewById(R.id.tvName);
+            number = itemView.findViewById(R.id.tvNumber);
+            foto = itemView.findViewById(R.id.iv_image_contactpaint);
+        }
+
+
+        public void asignarDatos(String incomingName, String incomingNumber, String incomingFoto){
+            name.setText(incomingName); // Metemos lo que le llegue
+            number.setText(incomingNumber);
+
+            if(incomingFoto == null){
+                foto.setImageResource(R.drawable.vector_person);
+            } else{
+                foto.setImageURI(Uri.parse(incomingFoto));
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/phone_contact_list/MainActivity.java
@@ -0,0 +1,98 @@
+package com.example.phone_contact_list;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.os.Build;
+import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView tvName;
+    private ArrayList<String> listDatosNombre, listDatosNumber, listDatosFoto;
+    private RecyclerView recycler;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tvName = findViewById(R.id.tvName);
+
+        // Creamos lo necesario para el recycler view
+        recycler = (RecyclerView) findViewById(R.id.rvContacts);
+        recycler.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
+        // También podemos hacer la lista Horizontal, o usar un grid layout para que lo enseñe a modo tabla
+        // (new GridLayoutManager(this, 2)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M &&
+                ContextCompat.checkSelfPermission(this, Manifest.permission.READ_CONTACTS)
+                        != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_CONTACTS}, 1);
+            Toast.makeText(this, "PERMISOS QUE FUERON PEDIDOS PORQUE NO HABÍA", Toast.LENGTH_SHORT).show();
+            getContacts();
+        } else {
+            Toast.makeText(this, "PERMISOS YA CONCENDIDOS", Toast.LENGTH_SHORT).show();
+            getContacts();
+        }
+    }
+
+    private void getContacts() {
+        listDatosNombre = new ArrayList<String>();
+        listDatosNumber = new ArrayList<String>();
+        listDatosFoto = new ArrayList<String>();
+
+        Cursor cursor = getContentResolver().query(
+                ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
+                null, null, null, null
+        );
+
+        if (cursor != null) {
+            int testName = cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME);
+            int testNumber = cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER);
+            int foto_uri = cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.PHOTO_URI);
+
+            if (testName >= 0 && testNumber >= 0) {
+
+                while (cursor.moveToNext()) {
+                    String name = cursor.getString(testName);
+                    String mobile = cursor.getString(testNumber);
+                    String foto = cursor.getString(foto_uri);
+
+                    listDatosNombre.add(name);
+                    listDatosNumber.add(mobile);
+
+                    if(foto!=null){
+                        listDatosFoto.add(foto);
+                        // Mandar datos al adapter datos
+                        AdapterDatos datos = new AdapterDatos (listDatosNombre, listDatosNumber, listDatosFoto);
+                        recycler.setAdapter(datos);
+
+                    } else { //Load default picture
+                        // Mandar datos al adapter datos
+                        listDatosFoto.add("file:///android_res/drawable/vector_person.png");
+
+                        AdapterDatos datos = new AdapterDatos (listDatosNombre, listDatosNumber, listDatosFoto);
+                        recycler.setAdapter(datos);
+                    }
+                }
+            } else {
+                tvName.setText("No se encontraron las columnas necesarias en el cursor");
+            }
+
+            cursor.close();
+        }
+    }
+}
