1f2144bd699152de67d10bbf07a59ceaaeedecf8
Juli <juliangasal46@gmail.com>
2023-08-28 10:41:21 +0200

    V.0.2.1: Mostly all done. TO_DO: No default pictures for no picture contacts

--- app/src/main/java/com/example/phone_contact_list/RecyclerViewSelectionContact.java
+++ app/src/main/java/com/example/phone_contact_list/RecyclerViewSelectionContact.java
@@ -1,5 +1,7 @@
 package com.example.phone_contact_list;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
@@ -7,6 +9,7 @@ import androidx.core.content.ContextCompat;
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
@@ -17,6 +20,11 @@ import android.widget.Toast;
 
 public class RecyclerViewSelectionContact extends AppCompatActivity {
 
+    private final ActivityResultLauncher<String> launcherPeticionPermisos =
+            registerForActivityResult(new ActivityResultContracts.RequestPermission(), isGranted -> {
+                String mensaje = isGranted ? "Permisos aceptados" : "Permisos denegados";
+                Toast.makeText(getApplicationContext(), mensaje, Toast.LENGTH_SHORT).show();
+            });
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -60,7 +68,15 @@ public class RecyclerViewSelectionContact extends AppCompatActivity {
         callPhoneNumber.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Pedir permisos para llamar
+                // First of all, check permission then call or send permission query
+                if (ContextCompat.checkSelfPermission(v.getContext(), Manifest.permission.CALL_PHONE) == PackageManager.PERMISSION_GRANTED) {
+                    Intent intent = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:" + number));
+                    startActivity(intent);
+
+                } else if (ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.CALL_PHONE) == PackageManager.PERMISSION_DENIED) {
+                    Toast.makeText(getApplicationContext(), "Necesitamos los permisos para que puedas llamar, por favor aceptalos", Toast.LENGTH_SHORT).show();
+                    launcherPeticionPermisos.launch(Manifest.permission.CALL_PHONE);
+                }
             }
         });
     }
